866ab0fe77204dd3f945f2e9fbf4852b9424861e
Genoyesil <10036635+xRuisu@users.noreply.github.com>
2022-03-20 20:28:24 -0400

    Slight Initial Update
    Minor changes to code

--- src/com/github/xruisu/drivingcalc_v2/About.java
+++ src/com/github/xruisu/drivingcalc_v2/About.java
@@ -1,5 +1,11 @@
 package com.github.xruisu.drivingcalc_v2;
 
+/**
+ * @author Louis Harris
+ * Driving Calculator
+ * Easy to use calculator for adding driving hours together.
+ */
+
 public class About {
 
 }
--- src/com/github/xruisu/drivingcalc_v2/Main.java
+++ src/com/github/xruisu/drivingcalc_v2/Main.java
@@ -1,10 +1,14 @@
 package com.github.xruisu.drivingcalc_v2;
 
 public class Main {
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) 
+	{
 		System.out.println("Enter the values on your log. When complete, type \"print\". Your results will be printed.");
 		System.out.println("Please use whole numbers! Acceptable Values: 12, 15, 30, 60");
-		while (References.isCalculating) {
+		
+		while (References.isCalculating) 
+		{
 			CalcTime.main(args);
 			TerminateProgram.kill(args);
 		} 
